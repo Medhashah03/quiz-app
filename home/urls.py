812db7e5d4a7bdcee5df20c9c91740fbda9a5a70
Medhashah03 <m0307shah@gmail.com>
2023-05-18 15:15:02 +0530

    urls change

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 from home.views import QuestionAPIView,viewCategories
 urlpatterns = [
     path('', views.home, name="home"),
-    path('api/get-quiz/', views.get_quiz, name="get_quiz"),
+    #path('api/get-quiz/', views.get_quiz, name="get_quiz"),
     path('api/get-category/', views.viewCategories, name="get_category"),
     path('api/questions/', QuestionAPIView.as_view(), name='questions'),
     path('quiz',views.quiz, name='quiz'),
